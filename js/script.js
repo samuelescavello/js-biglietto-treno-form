613d28a7df110cb10b4d23b6ab3d81a0c34a2ace
samuelescavello <scavellosamuele23@gmail.com>
2024-03-07 15:03:45 +0100

    ind

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// let numberKm = parseInt(prompt ('quanti km devi percorrere?'));
+// let ageUser = parseInt(prompt ('quanti anni hai?'));
+// console.log (numberKm, ageUser);
+// let priceKm = 0.21;
+// let youngUser = 20 / 100;
+// let oldUser = 40 / 100;
+// let totalPrice = priceKm * numberKm
+
+// if (!isNaN(ageUser) && !isNaN(numberKm)){
+//     if (ageUser < 18){
+//         totalPrice = (totalPrice - (totalPrice * youngUser))
+//         console.log(totalPrice)
+//     }else if (ageUser > 64){
+//         totalPrice = (totalPrice - (totalPrice * oldUser))
+//         console.log(totalPrice)
+//     }
+//     document.getElementById('prezzo').innerHTML = `il prezzo é ${totalPrice}`;
+// }
+// else{
+//     document.getElementById('prezzo').innerHTML = `non hai inserito i giusti dati!!`
+// }
